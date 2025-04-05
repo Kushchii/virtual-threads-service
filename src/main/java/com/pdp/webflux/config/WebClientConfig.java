4dfdb0ad737fe70410c3709e9f1851debc907d1f
Anton Kushchii <a.kushchii@tranzzo.com>
2025-04-05 18:34:58 +0300

    Add metrics and load tests

---
+++ src/main/java/com/pdp/webflux/config/WebClientConfig.java
@@ -0,0 +1,18 @@
+package com.pdp.webflux.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder, @Value("${service.client.transaction.url}") String url) {
+        return builder
+                .baseUrl(url)
+                .build();
+    }
+}
--- src/main/java/com/pdp/webflux/service/TransactionServiceImpl.java
+++ src/main/java/com/pdp/webflux/service/TransactionServiceImpl.java
@@ -8,6 +8,7 @@ import com.pdp.webflux.persistent.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import reactor.core.publisher.Mono;
@@ -26,10 +27,12 @@ public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
 
+    private final WebClient webClient;
+
     @Override
     public Mono<TransactionsResponse> transactions(TransactionsRequest request) {
         return processTransactionAndSave(request)
-                .thenReturn(new TransactionsResponse("Transaction processed successfully"));
+                .then(sendPostRequest(request));
     }
 
     private Mono<TransactionsEntity> processTransactionAndSave(TransactionsRequest request) {
@@ -37,5 +40,15 @@ public class TransactionServiceImpl implements TransactionService {
         return Mono.delay(Duration.ofSeconds(PROCESSING_DELAY_SECONDS))
                 .then(transactionRepository.save(entity))
                 .doOnSuccess(savedEntity -> log.info("Transaction entity saved: {}", savedEntity.getTransactionId()));
+    }
+
+    private Mono<TransactionsResponse> sendPostRequest(TransactionsRequest request) {
+        return webClient.post()
+                .uri("/process-payment")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(TransactionsResponse.class)
+                .doOnNext(response -> log.info("External service response: {}", response))
+                .doOnError(error -> log.error("Error during external POST call", error));
     }
 }
